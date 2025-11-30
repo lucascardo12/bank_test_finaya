9eeac618da0525ef8059fcde0e7092a908638136
lucas cardoso <lucas.cardo123@hotmail.com>
2025-11-30 02:37:49 -0300

    Add Spring configuration for service and controller beans
    Introduces ConfigurationBeans class to define beans for WalletsService, TransactionService, and WalletController, enabling dependency injection and configuration in the application context.

---
+++ src/main/java/com/lucas_cm/bank_test/configuration/ConfigurationBeans.java
@@ -0,0 +1,28 @@
+package com.lucas_cm.bank_test.configuration;
+
+import com.lucas_cm.bank_test.domain.repositories.TransactionRepository;
+import com.lucas_cm.bank_test.domain.repositories.WalletRepository;
+import com.lucas_cm.bank_test.domain.services.TransactionService;
+import com.lucas_cm.bank_test.domain.services.WalletsService;
+import com.lucas_cm.bank_test.infrastructure.controllers.WalletController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigurationBeans {
+
+    @Bean
+    public WalletsService walletsService(WalletRepository walletRepository) {
+        return new WalletsService(walletRepository);
+    }
+
+    @Bean
+    public TransactionService transactionService(TransactionRepository transactionRepository) {
+        return new TransactionService(transactionRepository);
+    }
+
+    @Bean
+    public WalletController walletController(WalletsService walletsService, TransactionService transactionService) {
+        return new WalletController(walletsService, transactionService);
+    }
+}
