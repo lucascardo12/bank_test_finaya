7d1595f3a2f222a9017004af9341c13eab3e453a
lucas cardoso <lucas.cardo123@hotmail.com>
2025-11-30 00:20:09 -0300

    Add custom exception handling for business errors
    Introduces BusinessException for domain-specific errors and a RestResponseEntityExceptionHandler to handle BusinessException and generic exceptions, returning appropriate HTTP responses with error details.

---
+++ src/main/java/com/lucas_cm/bank_test/configuration/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package com.lucas_cm.bank_test.configuration.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BusinessException extends RuntimeException {
+    private String errorCode;
+}
---
+++ src/main/java/com/lucas_cm/bank_test/configuration/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.lucas_cm.bank_test.configuration.exception;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@Slf4j
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<Object> handleBusinessError(BusinessException e) {
+        log.info("BusinessException", e);
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", e.getMessage());
+        body.put("error_code", e.getErrorCode());
+
+        return new ResponseEntity<>(body, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleInternalServerError(Exception e) {
+        log.error("Exception", e);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Ocorreu um erro no serviço");
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
