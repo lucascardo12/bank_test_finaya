1388d83f787a41042a7706578be32eac4f61bb2a
lucas cardoso <lucas.cardo123@hotmail.com>
2025-11-30 00:19:14 -0300

    Initialize Spring Boot bank-test project
    Set up a new Spring Boot project with Gradle, including basic configuration files, main application class, and a context load test. Added dependencies for Spring Boot, JPA, Lombok, and PostgreSQL, and configured .gitignore and .gitattributes for common IDEs and build artifacts.

---
+++ src/main/java/com/lucas_cm/bank_test/BankTestApplication.java
@@ -0,0 +1,13 @@
+package com.lucas_cm.bank_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTestApplication.class, args);
+	}
+
+}
